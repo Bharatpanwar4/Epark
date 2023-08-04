78840ff059f42f238b096a519348d2f981520bf5
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-08-04 23:58:51 +0530

    created cartPage

--- Frontend/src/pages/CartPage.jsx
+++ Frontend/src/pages/CartPage.jsx
@@ -1,30 +1,36 @@
-
-import { Link , useNavigate } from "react-router-dom"
-import { useDispatch,useSelector } from "react-redux"
-import {Row,Col,ListGroup,Image,Form,Button,Card} from 'react-bootstrap'
-import { FaTrash } from "react-icons/fa"
-import Message from "../components/Message"
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+import Message from "../components/Message";
 
 const CartPage = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const cart = useSelector((state)=>state.cart)
-    const {cartItems} = cart
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
-
-<Row>
-    <Col md={8}>
-        <h1 style={{marginBottom:'20px'}}>Shopping Cart</h1>
-        {cartItems.length === 0?(
-            <Message>Your cart is empty <Link to='/'>Go Back</Link></Message>
-        ):(
-            <ListGroup variant="flush">
-                Items
-            </ListGroup>
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">Items</ListGroup>
         )}
-    </Col>
-</Row>
-    )
-}
+      </Col>
+    </Row>
+  );
+};
 
-export default CartPage
+export default CartPage;
