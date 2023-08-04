00871735375e4cdde5893740554d594000493738
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-08-04 23:44:32 +0530

    created cart badge

--- Frontend/src/utils/cartUtils.js
+++ Frontend/src/utils/cartUtils.js
@@ -3,4 +3,20 @@ export const addDecimals = (num) =>{
 }
 
 
-export const updateCart =  
+export const updateCart = (state)=>{
+    // calculate items price
+    state.itemsPrice = addDecimals(state.cartItems.reduce((acc,item)=>acc + item.price * item.qty,0))
+
+    // calculate shipping price(if order is above $100 then free , else $10 shipping)
+    state.shippingPrice = addDecimals(state.itemsPrice>100 ?0 :10)
+    // calculate tax price
+    state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2))
+    // calculate total price
+state.totalPrice = (
+Number(state.itemsPrice) +Number(state.shippingPrice)+Number(state.taxPrice)
+).toFixed(2)
+
+//save to localstorage
+localStorage.setItem('cart',JSON.stringify(state))
+return state;
+}
