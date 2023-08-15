45b66e060565d752bef17e0a604381163549bce9
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-08-15 16:01:57 +0530

    production ready4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import path from 'path'
 import express from 'express'
 
+import {dirname} from 'path'
+
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 dotenv.config()
@@ -32,26 +35,33 @@ app.use('/api/orders',orderRoutes)
 app.use('/api/upload',uploadRoutes)
 app.get(`/api/config/paypal`,(req,res)=>res.send({clientId:process.env.PAYPAL_CLIENT_ID}))
 
-const __dirname = path.resolve();
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+// const __dirname = path.resolve();
+// app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
-// // production
-if(process.env.NODE_ENV === 'production'){
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static('/var/data/uploads'));
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname,'/Frontend/dist')))
 
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-}
-else{
-    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-    app.get('/',(req,res)=>{
-        res.send('Api is running')
-    })
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'/Frontend/build','index.html'))
+})
+// // // production
+// if(process.env.NODE_ENV === 'production'){
+//     const __dirname = path.resolve();
+//     app.use('/uploads', express.static('/var/data/uploads'));
+
+//     app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//     app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   );
+// }
+// else{
+//     app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//     app.get('/',(req,res)=>{
+//         res.send('Api is running')
+//     })
     
-}
+// }
 
 app.use(notFound)
 app.use(errorHandler)
