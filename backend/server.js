8ef5fed92bde0dae6253b1ccb936393c7a02cf59
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-08-15 19:28:49 +0530

    production ready test 17

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,7 @@ app.use(express.urlencoded({extended:true}))
 
 // cookie parser middleware
 app.use(cookieParser())
-const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.resolve(__dirname,'/frontend/build')))
+
 
 
 
@@ -34,27 +33,27 @@ app.use('/api/orders',orderRoutes)
 app.use('/api/upload',uploadRoutes)
 app.get(`/api/config/paypal`,(req,res)=>res.send({clientId:process.env.PAYPAL_CLIENT_ID}))
 
-// const __dirname = path.resolve();
-// app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+const __dirname = path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
 
 app.get('*',(req,res)=>{
   res.sendFile(path.resolve(__dirname,'/frontend/build','index.html'))
 })
 // production
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   );
-// } else {
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
 
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound)
 app.use(errorHandler)
