6bac3fe294daa291ecf78a62f3408958075fdd5e
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-09-01 00:31:18 +0530

    production ready test 2.1

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,46 +1,45 @@
-import { useState ,useEffect} from "react";
-import { Link ,useLocation,useNavigate} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer.jsx";
-import {useDispatch,useSelector} from 'react-redux';
-import Loader from '../components/Loader'
-import {useLoginMutation} from '../slices/usersApiSlice.jsx'
-import {setCredentials} from '../slices/authSlice.jsx';
-import {toast} from 'react-toastify'
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice.jsx";
+import { setCredentials } from "../slices/authSlice.jsx";
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
 
-const dispatch = useDispatch()
-const navigate = useNavigate()
-const [login,{isLoading}]=useLoginMutation()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
 
-const {userInfo} = useSelector((state)=>state.auth)
+  const { userInfo } = useSelector((state) => state.auth);
 
-const {search} = useLocation()
-const sp = new URLSearchParams(search)
-const redirect = sp.get('redirect') || '/'
-useEffect(()=>{
-if(userInfo){
-  navigate(redirect)
-}
-},[userInfo,redirect,navigate])
-  const submitHandler = async(e) => {
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate]);
+  const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await login({email,password}).unwrap()
-      dispatch(setCredentials({...res,}))
-      navigate(redirect)
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
     } catch (err) {
-      toast.error(err?.data?.message || err.error )
+      toast.error(err?.data?.message || err.error);
     }
     // console.log("submit");
   };
 
-
-  return (<FormContainer>
+  return (
+    <FormContainer>
       <h1>Sign In</h1>
       <Form onSubmit={submitHandler}>
         {/* email */}
@@ -67,32 +66,27 @@ if(userInfo){
           ></Form.Control>
         </Form.Group>
         {/* sign in button */}
-        <Button type="submit" variant="dark" className="mt-2 w-100" disabled={isLoading}>
+        <Button
+          type="submit"
+          variant="dark"
+          className="mt-2 w-100"
+          disabled={isLoading}
+        >
           Sign In
         </Button>
-        {isLoading && <Loader/>}
+        {isLoading && <Loader />}
       </Form>
 
       <Row className="py-3">
         <Col>
-          New Customer? <Link to={redirect? `/register?redirect=${redirect}`:'/register'}>Register</Link>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
         </Col>
       </Row>
     </FormContainer>
-   
-  )
+  );
 };
 
 export default LoginPage;
-
-
-
-
-
-
-
-
-
-
-
-
--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -1,62 +1,58 @@
-import { useState ,useEffect} from "react";
-import { Link ,useLocation,useNavigate} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer.jsx";
-import {useDispatch,useSelector} from 'react-redux';
-import Loader from '../components/Loader'
-import {useRegisterMutation} from '../slices/usersApiSlice.jsx'
-import {setCredentials} from '../slices/authSlice.jsx';
-import {toast} from 'react-toastify'
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../slices/usersApiSlice.jsx";
+import { setCredentials } from "../slices/authSlice.jsx";
+import { toast } from "react-toastify";
 
 const RegisterPage = () => {
-    const [name,setName] =useState("")
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [register, { isLoading }] = useRegisterMutation();
 
-const dispatch = useDispatch()
-const navigate = useNavigate()
-const [register,{isLoading}]=useRegisterMutation()
+  const { userInfo } = useSelector((state) => state.auth);
 
-const {userInfo} = useSelector((state)=>state.auth)
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate]);
 
-const {search} = useLocation()
-const sp = new URLSearchParams(search)
-const redirect = sp.get('redirect') || '/'
-useEffect(()=>{
-if(userInfo){
-  navigate(redirect)
-}
-},[userInfo,redirect,navigate])
-
-  const submitHandler = async(e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    if(password !== confirmPassword){
-        toast.error('Passwords do not match')
-        return;
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
     }
-    else{
 
-        try {
-            const res = await register({name,email,password}).unwrap()
-            dispatch(setCredentials({...res,}))
-            navigate(redirect)
-          } catch (err) {
-            toast.error(err?.data?.message || err.error )
-          }
-    }
-   
     // console.log("submit");
   };
 
-
-  return (<FormContainer>
+  return (
+    <FormContainer>
       <h1>Sign Up</h1>
       <Form onSubmit={submitHandler}>
-{/* name */}
-<Form.Group controlId="name" className="my-3">
+        {/* name */}
+        <Form.Group controlId="name" className="my-3">
           <Form.Label>Name</Form.Label>
           <Form.Control
             type="text"
@@ -65,7 +61,6 @@ if(userInfo){
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
-
 
         {/* email */}
         <Form.Group controlId="email" className="my-3">
@@ -90,9 +85,9 @@ if(userInfo){
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-          {/*confirm Password */}
+        {/*confirm Password */}
 
-          <Form.Group controlId="confirmPassword" className="my-3">
+        <Form.Group controlId="confirmPassword" className="my-3">
           <Form.Label> Confirm Password</Form.Label>
           <Form.Control
             type="password"
@@ -102,32 +97,27 @@ if(userInfo){
           ></Form.Control>
         </Form.Group>
         {/* sign up button */}
-        <Button type="submit" variant="dark" className="mt-2 w-100" disabled={isLoading}>
+        <Button
+          type="submit"
+          variant="dark"
+          className="mt-2 w-100"
+          disabled={isLoading}
+        >
           Register
         </Button>
-        {isLoading && <Loader/>}
+        {isLoading && <Loader />}
       </Form>
 
       <Row className="py-3">
         <Col>
-          All have an account? <Link to={redirect? `/login?redirect=${redirect}`:'/login'}>Login</Link>
+          All have an account?{" "}
+          <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+            Login
+          </Link>
         </Col>
       </Row>
     </FormContainer>
-   
-  )
+  );
 };
 
 export default RegisterPage;
-
-
-
-
-
-
-
-
-
-
-
-
